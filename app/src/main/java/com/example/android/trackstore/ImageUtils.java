1f45792d7fe0e178f064fba4069ac0560b1cb60e
Rahul <ryan.kumar681@gmail.com>
2017-10-09 00:23:38 +0530

    Eliminated redundent method

--- app/src/main/java/com/example/android/trackstore/ImageUtils.java
+++ app/src/main/java/com/example/android/trackstore/ImageUtils.java
@@ -1,12 +1,8 @@
 package com.example.android.trackstore;
 
-
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 public class ImageUtils {
 
@@ -21,16 +17,4 @@ public class ImageUtils {
         Bitmap bitmap = BitmapFactory.decodeByteArray(image, 0, image.length);
         return bitmap;
     }
-
-//    public static byte[] getBytes(InputStream inputStream) throws IOException {
-//        ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
-//        int bufferSize = 1024;
-//        byte[] buffer = new byte[bufferSize];
-//
-//        int len = 0;
-//        while((len = inputStream.read(buffer)) !=1){
-//            byteBuffer.write(buffer, 0, len);
-//        }
-//        return byteBuffer.toByteArray()
-//  }
 }
